b8180a3debf2042cbd6b26cd54de2209ef6c7023
Jorge.Garcia <jorge.a.garcia12@gmail.com>
2019-10-02 06:18:27 -0700

    more stuff

---
+++ day3/script.js
@@ -0,0 +1,6 @@
+//let value1 = document.querySelector('.cities');
+var blah = document.getElementById('#yeye');
+let thing = document.getElementsByClassName(':cities');
+//blah.innerHTML = 'this is some text';
+//console.log(thing[0].value);
+console.log(blah);
